327f1aa8bbd6c9073ccf06e2938ebfce7a48e877
Cavit Ertuğrul Sırt <ertugrul@ortakapp.com>
2025-12-12 22:57:02 +0300

    chore: chore

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -8,7 +8,6 @@ import {
 import { convertDocxToPdf } from "./utils/docx-to-pdf.js";
 import { getFilesAndFoldersInDirectory } from "./utils/get-files-and-folders-in-directory.js";
 import { openFile } from "./utils/open-file.js";
-import { getInitializedFuzzyFinder } from "./utils/get-initialized-fuzzy-finder.js";
 import { expandHome } from "./utils/expand-home.js";
 import { base64ImageToTempPath } from "./utils/base64-image-to-temp-path.js";
 import { captureRect } from "./utils/capture-rect.js";
@@ -112,15 +111,6 @@ app.on("ready", () => {
   ipcHandle("docxToPdf", (filePath: string) =>
     convertDocxToPdf(filePath, undefined, { copyBase64ToClipboard: true }),
   );
-
-  getInitializedFuzzyFinder({
-    filePathOptions: { extensions: ["docx"] },
-  }).then((fuzzyFinder) => {
-    ipcHandle("fuzzyFind", (query) => {
-      const results = fuzzyFinder.search(query, { limit: 50 });
-      return results.map((result) => result.item.actual);
-    });
-  });
 
   ipcHandle("getFilesAndFoldersInDirectory", getFilesAndFoldersInDirectory);
   ipcHandle("openFile", openFile);
