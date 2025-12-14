c8a0d33f8eef0432b43ed8fff6f5899e0cda748f
ertucode <ertugrulsirtt@gmail.com>
2025-12-14 18:50:38 +0300

    refac

--- src/ui/features/file-browser/components/FileBrowserNavigationAndInputSection.tsx
+++ src/ui/features/file-browser/components/FileBrowserNavigationAndInputSection.tsx
@@ -1,29 +1,41 @@
-import {
-  FuzzyFinderInput,
-  useFuzzyFinder,
-} from "@/lib/libs/fuzzy-find/FuzzyFinderInput";
 import { ArrowLeftIcon, ArrowRightIcon, ArrowUpIcon } from "lucide-react";
 import { FolderBreadcrumb } from "./FolderBreadcrumb";
 import { useSelector } from "@xstate/store/react";
+import { useEffect, useRef } from "react";
 import {
   directoryStore,
   directoryHelpers,
   selectHasNext,
   selectHasPrev,
+  selectFuzzyQuery,
+  selectFilteredDirectoryData,
 } from "../directory";
-import { GetFilesAndFoldersInDirectoryItem } from "@common/Contracts";
 
-export type FileBrowserNavigationAndInputSectionProps = {
-  fuzzy: ReturnType<typeof useFuzzyFinder<GetFilesAndFoldersInDirectoryItem>>;
-};
-
-export function FileBrowserNavigationAndInputSection({
-  fuzzy,
-}: FileBrowserNavigationAndInputSectionProps) {
+export function FileBrowserNavigationAndInputSection() {
   const navigationButtonClassName = "btn btn-xs btn-soft btn-info join-item";
   const navigationButtonIconClassName = "size-4";
   const hasNext = useSelector(directoryStore, selectHasNext);
   const hasPrev = useSelector(directoryStore, selectHasPrev);
+  const fuzzyQuery = useSelector(directoryStore, selectFuzzyQuery);
+  const filteredData = useSelector(directoryStore, selectFilteredDirectoryData);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    return directoryStore.on("focusFuzzyInput", ({ e }) => {
+      e.preventDefault();
+      inputRef.current?.focus();
+      const query = directoryStore.getSnapshot().context.fuzzyQuery;
+      if (query) {
+        setTimeout(() => {
+          const i = inputRef.current;
+          if (!i) return;
+          i.selectionStart = 0;
+          i.selectionEnd = query.length;
+        }, 100);
+      }
+    }).unsubscribe;
+  }, []);
 
   return (
     <div className="join items-center mb-2">
@@ -50,10 +62,34 @@ export function FileBrowserNavigationAndInputSection({
       <div className="flex-1 join-item px-2 overflow-x-auto">
         <FolderBreadcrumb />
       </div>
-      <FuzzyFinderInput
-        fuzzy={fuzzy}
-        className="w-48 min-[1000px]:w-80 join-item"
-        onEnter={() => directoryHelpers.openSelectedItem(fuzzy.results)}
+      <input
+        id="fuzzy-finder-input"
+        type="text"
+        ref={inputRef}
+        className="input text-sm h-6 max-w-50 w-48 min-[1000px]:w-80 join-item"
+        placeholder="Search... (/)"
+        value={fuzzyQuery}
+        onChange={(e) => {
+          directoryHelpers.setFuzzyQuery(e.target.value);
+        }}
+        onKeyDown={(e) => {
+          if (e.key === "Escape") {
+            directoryHelpers.clearFuzzyQuery();
+            e.currentTarget.blur();
+          }
+          if (e.key === "j" && e.ctrlKey)
+            directoryHelpers.setSelection((h) =>
+              Math.min(h + 1, filteredData.length - 1),
+            );
+          if (e.key === "k" && e.ctrlKey)
+            directoryHelpers.setSelection((h) => Math.max(h - 1, 0));
+
+          if (e.key === "Enter") {
+            directoryHelpers.openSelectedItem(filteredData);
+            directoryHelpers.clearFuzzyQuery();
+            e.currentTarget.blur();
+          }
+        }}
       />
     </div>
   );
