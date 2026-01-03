bd4720a7df5285fa9a0ac1dd33c2613a6b010d1c
Cavit Ertuğrul Sırt <ertugrul@ortakapp.com>
2026-01-04 02:04:31 +0300

    fast build

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,29 +1,29 @@
 // import "../../wdyr/wdyr.js";
-import "./App.css";
-import { FlexLayoutManager } from "./features/tile-manager";
-import { ConfirmationRenderer } from "./lib/components/confirmation";
-import { ToastRenderer } from "./lib/components/toast";
-import { TaskMonitor } from "./features/TaskMonitor";
-import { subscribeToTasks } from "./features/taskSubscription";
-import { useCallback } from "react";
-import { useWindowFocus } from "./lib/hooks/useWindowFocus";
-import { directoryStore } from "./features/file-browser/directoryStore/directory";
-import { directoryHelpers } from "./features/file-browser/directoryStore/directoryHelpers";
-import { subscribeToGenericEvents } from "./features/genericEventListener";
+import './App.css'
+import { FlexLayoutManager } from './features/tile-manager'
+import { ConfirmationRenderer } from './lib/components/confirmation'
+import { ToastRenderer } from './lib/components/toast'
+import { TaskMonitor } from './features/TaskMonitor'
+import { subscribeToTasks } from './features/taskSubscription'
+import { useCallback } from 'react'
+import { useWindowFocus } from './lib/hooks/useWindowFocus'
+import { directoryStore } from './features/file-browser/directoryStore/directory'
+import { directoryHelpers } from './features/file-browser/directoryStore/directoryHelpers'
+import { subscribeToGenericEvents } from './features/genericEventListener'
 
-subscribeToTasks();
-subscribeToGenericEvents();
+subscribeToTasks()
+subscribeToGenericEvents()
 
 function App() {
   const handleWindowFocus = useCallback(() => {
-    const context = directoryStore.getSnapshot().context;
+    const context = directoryStore.getSnapshot().context
 
     for (const directoryId of context.directoryOrder) {
-      directoryHelpers.reloadIfChanged(directoryId);
+      directoryHelpers.reloadIfChanged(directoryId)
     }
-  }, []);
+  }, [])
 
-  useWindowFocus(handleWindowFocus);
+  useWindowFocus(handleWindowFocus)
 
   return (
     <>
@@ -33,7 +33,7 @@ function App() {
       <FlexLayoutManager />
       <TaskMonitor />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
